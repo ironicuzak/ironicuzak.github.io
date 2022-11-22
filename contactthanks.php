3f8892aa2bcbeba37ab385f4db23bbcfb3f7680a
ironicuzak <118162792+ironicuzak@users.noreply.github.com>
2022-11-22 19:46:50 +0100

    uploade complet

---
+++ contactthanks.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset=utf-8>
+<!-- simplified version; works on legacy browsers -->
+<title>Michaela Henniker-Heaton | Artisan décorateur</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen">
+<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
+<script type="text/javascript">
+
+   $(document).ready(function(){
+           $("#fadein").fadeIn(1000);
+   });
+
+</script>
+<body>
+<div id="global">
+<header>
+    <logo>
+    	<a href="index.html"><img src="images/logo_mhh.png" alt="" width="250px" height="35px" style="border:none;"/></a>
+	</logo>
+	<nav>  
+    <ul>  
+        <li><a href="index.html">HOME</a></li>  
+        <li><a href="showcase.html">SHOWCASE</a></li>  
+        <li><a href="press.html">PRESS</a></li>  
+        <li><a href="about.html">ABOUT</a></li>  
+        <li><a href="contact.html">CONTACT</a></li> 
+    </ul>  
+	</nav> 
+</header>
+<div id="fadein" style="display: none;">
+<section>
+<article>
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="contact.html">Back to Contact Form</a></p>
+</article>
+</section></div>
+<footer>
+<article>Réalisation de décors du XVIII à nos jours:<br />
+Peinture décoratives<span style="color:#009ab2;"> / </span>trompe l'oeil<span style="color:#009ab2;"> / </span>toutes patines<span style="color:#009ab2;"> / </span>imitations marbres, bois & pierres</article></footer>
+<note>©Michaela Henniker-Heaton 2010 | details.ch</note>
+</div>
+</body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function() {
+// OPACITY OF BUTTON SET TO 50%
+$(".latest_img").css("opacity","1.0");
+		
+// ON MOUSE OVER
+$(".latest_img").hover(function () {
+										  
+// SET OPACITY TO 100%
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+		
+// ON MOUSE OUT
+function () {
+			
+// SET OPACITY BACK TO 50%
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+});
